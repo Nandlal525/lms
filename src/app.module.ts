77f63a485d9dcc20e669d53696d7f10efe16ef70
Nandlal <nandlaly6263525@gmail.com>
2025-04-01 12:59:41 +0545

    third commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
+import { UsersModule } from './users/users.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [MembersModule],
+  imports: [MembersModule, UsersModule, TransactionsModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
